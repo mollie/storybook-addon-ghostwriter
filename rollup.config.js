4b4e7df0eaaf51488b1b1efe99e666cef0b14097
Marino Linderhof <marino@mollie.com>
2018-06-13 12:05:15 +0200

    remove css rollup cause we don't use it

--- rollup.config.js
+++ rollup.config.js
@@ -1,15 +1,13 @@
 var rollup = require('rollup');
 var babel = require('rollup-plugin-babel');
-var css = require('rollup-plugin-css-only');
 var copy = require('rollup-plugin-copy');
 
 export default [
   {
     input: 'src/index.js',
     plugins: [
-      css({ output: 'dist/styles.css' }),
       babel(),
-      copy({ 'src/mollie-logo.svg': 'dist/mollie-logo.svg' }),
+      copy({ 'src/mollie-logo.svg': 'dist/mollie-logo.svg', 'src/styles.css': 'dist/styles.css' }),
     ],
     output: {
       file: 'dist/index.js',
